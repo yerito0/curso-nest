4571ba304110408fa9279fe013a0b1235038accd
Yeri Silva <yeri.se17@hotmail.com>
2023-04-02 17:39:36 -0400

    Sección 8, finalizada clase 91, antes del ejercicio 92

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
   async fillBd() {
-    const { data } = await this.axios.get(
+    const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon/?limit=650',
     );
-    return data;
+
+    data.results.forEach(({name, url}) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length -2];
+    });
+
+    return data.results;
   }
 }
